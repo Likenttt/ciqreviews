56fb9ac586e9603ae91242e250431d18a1440d0e
lichuanyi <chuanyi.work@gmail.com>
2022-12-28 00:03:51 +0800

    fix owner env

--- data-scripts/create-issue.py
+++ data-scripts/create-issue.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 TOKEN = os.environ['GITHUB_TOKEN']
 g = Github(TOKEN)
-repo = g.get_repo(os.environ['OWNER'] + '/' + os.environ['REPO_NAME'])
+repo = g.get_repo(os.environ['REPO_NAME'])
 
 # 2 kinds of issues
 # 1. all developers' id-app dict
