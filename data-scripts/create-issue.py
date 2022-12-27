d3a113daf89c10d58116fd3ed2c78a327d3098d1
lichuanyi <chuanyi.work@gmail.com>
2022-12-27 23:48:36 +0800

    update app repo name

--- data-scripts/create-issue.py
+++ data-scripts/create-issue.py
@@ -6,7 +6,8 @@ from datetime import datetime
 
 TOKEN = os.environ['GITHUB_TOKEN']
 g = Github(TOKEN)
-repo = g.get_repo(os.environ['OWNER'] + '/' + os.environ['REPO_NAME'])
+repo_name = os.environ['OWNER'] + '/' + os.environ['REPO_NAME']
+repo = g.get_repo(repo_name)
 
 # 2 kinds of issues
 # 1. all developers' id-app dict
